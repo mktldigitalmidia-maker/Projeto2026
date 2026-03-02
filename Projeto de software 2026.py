fc3951d15532e0f44f0ffc4140dbde832463cc4a
mktldigitalmidia-maker <mktl.digitalmidia@gmail.com>
2026-03-02 17:11:11 -0300

    Add files via upload

---
+++ Projeto de software 2026.py
@@ -0,0 +1,5 @@
+nome = input('Digite seu nome: ')
+idade = input('Digite sua idade: ')
+peso = input('Digite seu peso: ')
+print("Você se chama {},tem {}anos e pesa {}kg certo?".format(nome,idade,peso))
+
