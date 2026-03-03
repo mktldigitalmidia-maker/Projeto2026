95f15f9a7db25516b9901b17be518d745040a012
mktldigitalmidia-maker <mktl.digitalmidia@gmail.com>
2026-03-03 16:24:12 -0300

    Primeira  alteração
    Alteração de "peso" para CPF nas informações do usuário

--- Projeto de software 2026.py
+++ Projeto de software 2026.py
@@ -1,5 +1,8 @@
 nome = input('Digite seu nome: ')
 idade = input('Digite sua idade: ')
-peso = input('Digite seu peso: ')
-print("Você se chama {},tem {}anos e pesa {}kg certo?".format(nome,idade,peso))
+CPF = input('Digite seu CPF: ')
+print("Você se chama {},tem {} anos e seu CPF é {} certo?".format(nome,idade,CPF))
+
+
+
 
